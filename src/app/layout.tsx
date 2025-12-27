c6a163edd962eb5706182f67ed29d1b37fe20857
aishiek <whizaleem@yahoo.com>
2025-12-27 10:44:32 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     siteName: "iHafidh",
     images: [
       {
-        url: "https://ihafidh-website.vercel.app/og-image.png",
+        url: "https://ihafidh-website.vercel.app/banner.png",
         width: 1200,
         height: 630,
         alt: "iHafidh App - Master Quran Memorization",
